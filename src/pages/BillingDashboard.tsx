e0b3b0009bd502ee6254e47fbf69fe16c8bd5c56
jSasaki <takatsugu.sasaki@anti-pattern.co.jp>
2025-09-03 16:11:07 +0900

    adjust spacing

--- src/pages/BillingDashboard.tsx
+++ src/pages/BillingDashboard.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useNavigate,useLocation } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { API_ENDPOINT, LOGIN_URL } from "../const";
 import { idTokenCheck, randomUnixBetween, handleUserListClick } from "../utils";
 import {
